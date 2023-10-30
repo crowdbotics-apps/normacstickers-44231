280be12cabcb142a3ce83d56caa3765f51f39f64
Crowdbotics <team@crowdbotics.com>
2023-10-30 21:45:47 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "normacstickers-44231",
+  url: "https://normacstickers-44231.botics.co",
+  api: "https://normacstickers-44231.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
